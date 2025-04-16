7e7e558cb258ad2bf553b914a672ea40889843fa
katsup <brett_7y7@yahoo.ca>
2025-04-16 14:53:05 +0900

    Add dockerized versions of server and client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // needed for docker container in development
+    port: 5173,
+    strictPort: true,
+  }
 })
